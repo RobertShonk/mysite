ad5992cbd53e16c3408dca1ca58321d782313ef6
Robert Shonk <rshonkcode@gmail.com>
2023-06-17 17:27:22 -0700

    added bootstrap and added some content to index.html

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main.apps.MainConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
